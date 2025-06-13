ed65417fe22291e3a175ec0830d34611e959345f
worldOfVerdure <perspicaciousdotard@gmail.com>
2025-06-12 20:07:31 -0400

    Learn about typing and props + children, as well as FC PropsWithChildren

--- src/App.tsx
+++ src/App.tsx
@@ -1,3 +1,15 @@
+import CourseGoal from "./components/CourseGoal.tsx";
+import goalsImg from "./assets/goals.jpg";
+import Header from "./components/Header.tsx";
+
 export default function App() {
-  return <h1>Let's get started!</h1>;
+  return (
+    <main>
+      {/* Goal: build header component */}
+      <Header image={{src: goalsImg, alt: "A list of goals"}} >
+        <h1>Your Course Goals</h1>
+      </Header>
+      <CourseGoal title="Learn React + TS" description="Learn from the ground up." />
+    </main>
+  );
 }
--- src/components/CourseGoal.tsx
+++ src/components/CourseGoal.tsx
@@ -1,11 +1,78 @@
-export default function CourseGoal() {
+// export default function CourseGoal(props: {
+//   title: string;
+//   description: string;
+//   }) {
+//   return (
+//     <article>
+//       <div>
+//         <h2>TITLE</h2>
+//         <p>DESCRIPTION</p>
+//       </div>
+//       <button>Delete</button>
+//     </article>
+//   );
+// }
+/** */
+// export default function CourseGoal({title, description}: {
+//   title: string;
+//   description: string;
+//   }) {
+//   return (
+//     <article>
+//       <div>
+//         <h2>{title}</h2>
+//         <p>{description}</p>
+//       </div>
+//       <button>Delete</button>
+//     </article>
+//   );
+// }
+
+/*Both the above lines of code can become hard to read quickly. See below for a solution.*/
+// import { type ReactNode } from "react";
+
+// type CourseGoalProps = {
+//   description: string;
+//   title: string;
+// }
+
+/*
+If children prop is passed, used the below type.
+*/
+// type CourseGoalProps = {
+//   title: string;
+//   children: ReactNode;
+// }
+
+// export default function CourseGoal({title, description}: CourseGoalProps) {
+//   return (
+//     <article>
+//       <div>
+//         <h2>{title}</h2>
+//         <p>{description}</p>
+//       </div>
+//       <button>Delete</button>
+//     </article>
+//   );
+// }
+
+/* My prefered way to type annotate a component using FC, functional component: */
+import {type FC} from "react";
+type CourseGoalProps = {
+  title: string;
+  description: string;
+}
+// <CourseGoalProps> and {title, description} are connected.
+const CourseGoal: FC<CourseGoalProps> = ({title, description}) => {
   return (
     <article>
       <div>
-        <h2>TITLE</h2>
-        <p>DESCRIPTION</p>
+        <h2>{title}</h2>
+        <p>{description}</p>
       </div>
       <button>Delete</button>
     </article>
   );
 }
+
+export default CourseGoal;
